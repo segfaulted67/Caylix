1857c5aa2337d4da4b51d84d36f86878a3225c1b
larry <segfaulted67@gmail.com>
2025-12-12 23:32:27 +0530

    Added nib.h to build examples

---
+++ test/nib.c
@@ -0,0 +1,12 @@
+#define NIB_IMPLEMENTATION
+#define ANSI_ENABLE
+#include "../build/nib.h"
+
+
+int main()
+{
+  CMD cmd = { 0 };
+  nib_cmd_append(&cmd, "gcc", "-std=c99", "-o", "./example", "./example.c", "-lm");
+  if(!nib_cmd_run(cmd)) return 1;
+  return 0;
+}
